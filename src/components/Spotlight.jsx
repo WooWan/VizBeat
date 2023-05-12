2dec882d5f50c9ea9f7c7b68fa533e72e99e4171
WooWan <woohobi@gmail.com>
2023-05-12 14:21:11 +0900

    Lint prettier 적용

--- src/components/Spotlight.jsx
+++ src/components/Spotlight.jsx
@@ -3,6 +3,14 @@ import { useRef } from 'react';
 const Spotlight = (props) => {
   const targetRef = useRef();
 
-  return <spotLight ref={targetRef} position={props.position} angle={props.angle} penumbra={props.angle} castShadow />;
+  return (
+    <spotLight
+      ref={targetRef}
+      position={props.position}
+      angle={props.angle}
+      penumbra={props.angle}
+      castShadow
+    />
+  );
 };
 export default Spotlight;
