cb62fffe245046d5c820c31c1ea4b2fbb9a1f2c4
WooWan <woohobi@gmail.com>
2023-05-12 14:13:11 +0900

    Remove music list 중복

--- src/components/MusicList.tsx
+++ src/components/MusicList.tsx
@@ -2,40 +2,31 @@ import React, { useRef } from 'react';
 import * as THREE from 'three';
 import { useScroll } from '@react-three/drei';
 import Music from '@/components/Music';
-import {useFrame, useThree} from '@react-three/fiber';
-
-const list = [
-  '/album/avril.png',
-  '/album/blonde.jpeg',
-  '/album/daft.jpeg',
-  '/album/guns.jpeg',
-  '/album/nevermind.jpg.webp',
-  '/album/aimyon.jpeg',
-  '/album/billie.webp',
-  '/album/blackpink.jpeg',
-  '/album/breeders.jpeg',
-  '/album/damn.jpeg',
-  '/album/newjeans.webp',
-  '/album/Q.jpeg'
-];
+import { useFrame, useThree } from '@react-three/fiber';
 
 type Props = {
   handleClick: (idx: number) => void;
   selectedIdx: null | number;
   setSelectedIdx: React.Dispatch<React.SetStateAction<null | number>>;
-}
-const MusicList = ({handleClick, setSelectedIdx, selectedIdx} : Props) => {
+  musicList: string[];
+};
+const MusicList = ({
+  handleClick,
+  setSelectedIdx,
+  selectedIdx,
+  musicList
+}: Props) => {
   const groupRef = useRef<THREE.Group>(null!);
-  const three = useThree()
+  const three = useThree();
   const scroll = useScroll();
 
   useFrame(() => {
-    groupRef.current.position.y =  scroll.offset * three.viewport.height * 0.25;
+    groupRef.current.position.y = scroll.offset * three.viewport.height * 0.25;
   });
 
   return (
     <group ref={groupRef}>
-      {list.map((url, i) => (
+      {musicList.map((url, i) => (
         <Music
           key={i}
           url={url}
--- src/pages/musics/index.tsx
+++ src/pages/musics/index.tsx
@@ -1,9 +1,9 @@
-import React, {  useRef, useState } from 'react';
-import {Canvas} from '@react-three/fiber';
-import {ScrollControls} from '@react-three/drei';
-import MusicList from "@/components/MusicList";
+import React, { useRef, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { ScrollControls } from '@react-three/drei';
+import MusicList from '@/components/MusicList';
 
-const list = [
+const musicList = [
   '/album/avril.png',
   '/album/blonde.jpeg',
   '/album/daft.jpeg',
@@ -38,54 +38,56 @@ const Index = () => {
   };
 
   return (
-      <div className={'grid grid-cols-[400px_minmax(900px,_1fr)] h-screen'}>
-        <div className={'bg-white bg-opacity-90 px-6'}>
-          <h1>Music Title</h1>
-          <img src="/album/aimyon.jpeg"/>
-          Selected Music : <audio id="vocal" controls src=""></audio>
-          <br/>
-          <span>My List</span>
-          <ul
-              className={
-                'overflow-y-scroll h-32 snap-y snap-mandatory bg-black bg-opacity-90 py-4 flex flex-col items-center  scrollbar scrollbar-thumb-red-400'
-              }
-          >
-            {list.map((title, index) => (
-                <li
-                    key={index}
-                    className={
-                      'py-2 border-y-[1px] border-y-amber-50 w-[250px] flex justify-center cursor-pointer snap-center text-white'
-                    }
-                    ref={(el) => (listRefs.current[index] = el)}
-                    onClick={() => handleClick(index)}
-                >
-                  {title}
-                </li>
-            ))}
-          </ul>
-          <button>+ add</button>
-        </div>
-        <Canvas
-            className="scrollbar scrollbar-thumb-red-400 h-32"
-            camera={{
-              zoom: 1.7,
-              position: [15, 5, 0],
-              fov: 100,
-              near: 0.1,
-              far: 1000
-            }}
+    <div className={'grid grid-cols-[400px_minmax(900px,_1fr)] h-screen'}>
+      <div className={'bg-white bg-opacity-90 px-6'}>
+        <h1>Music Title</h1>
+        <img src="/album/aimyon.jpeg" />
+        Selected Music : <audio id="vocal" controls src=""></audio>
+        <br />
+        <span>My List</span>
+        <ul
+          className={
+            'overflow-y-scroll h-32 snap-y snap-mandatory bg-black bg-opacity-90 py-4 flex flex-col items-center  scrollbar scrollbar-thumb-red-400'
+          }
         >
-          <color attach="background" args={['white']}/>
-          {/* style for hide scroll bar */}
-          <ScrollControls
-              damping={0}
-              // style={{ left: '15px' }}
-          >
-            <MusicList handleClick={handleClick} selectedIdx={selectedIdx} setSelectedIdx={setSelectedIdx}/>
-          </ScrollControls>
-          <ambientLight/>
-        </Canvas>
+          {musicList.map((title, index) => (
+            <li
+              key={index}
+              className={
+                'py-2 border-y-[1px] border-y-amber-50 w-[250px] flex justify-center cursor-pointer snap-center text-white'
+              }
+              ref={(el) => (listRefs.current[index] = el)}
+              onClick={() => handleClick(index)}
+            >
+              {title.split('/')[2].split('.')[0]}
+            </li>
+          ))}
+        </ul>
+        <button>+ add</button>
       </div>
+      <Canvas
+        className="scrollbar scrollbar-thumb-red-400 h-32"
+        camera={{
+          zoom: 1.7,
+          position: [15, 5, 0],
+          fov: 100,
+          near: 0.1,
+          far: 1000
+        }}
+      >
+        <color attach="background" args={['white']} />
+        {/* style for hide scroll bar */}
+        <ScrollControls damping={0} style={{ left: '15px' }}>
+          <MusicList
+            handleClick={handleClick}
+            selectedIdx={selectedIdx}
+            setSelectedIdx={setSelectedIdx}
+            musicList={musicList}
+          />
+        </ScrollControls>
+        <ambientLight />
+      </Canvas>
+    </div>
   );
 };
 
