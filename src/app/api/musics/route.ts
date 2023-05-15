a5b22aa9778c851e31b137dae9654c1f353eca1c
WooWan <woohobi@gmail.com>
2023-05-15 21:58:27 +0900

    Fix prisma export

--- src/app/api/musics/route.ts
+++ src/app/api/musics/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-export { prisma } from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 
 export async function GET() {
   const res = await prisma.music.findMany();
