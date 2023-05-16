89d1ab0fcf8bdd14577bc8ea8e6e4aa6cd8f9260
WooWan <woohobi@gmail.com>
2023-05-17 01:09:15 +0900

    Add home section components

---
+++ src/components/CustomTexts.tsx
@@ -0,0 +1,32 @@
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '@/utils/motion';
+import React from 'react';
+
+type Props = {
+  title: string;
+  textStyles?: string;
+};
+export const TypingText = ({ title, textStyles }: Props) => (
+  <motion.p variants={textContainer} className={`font-normal text-[14px] text-secondary-white ${textStyles}`}>
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
+    ))}
+  </motion.p>
+);
+
+type TitleProps = {
+  title: string | React.ReactNode;
+  textStyles?: string;
+};
+export const TitleText = ({ title, textStyles }: TitleProps) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
+);
