9d89380f26b31d374088200303fabf26b9e1b515
WooWan <woohobi@gmail.com>
2023-05-18 17:26:53 +0900

    Refactor selected index, selected music 통합

--- src/components/MusicList.tsx
+++ src/components/MusicList.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { SetStateAction, useRef } from 'react';
 import * as THREE from 'three';
 import { useScroll } from '@react-three/drei';
 import MusicCard from '@/components/Music';
@@ -6,13 +6,13 @@ import { useFrame, useThree } from '@react-three/fiber';
 import { Music } from '@prisma/client';
 
 type Props = {
-  handleClick: (idx: number) => void;
-  selectedIdx: null | number;
-  setSelectedIdx: React.Dispatch<React.SetStateAction<null | number>>;
+  handleClick: (id: string) => void;
   musicList: Music[];
+  selectedMusic: Music | null;
+  setSelectedMusic: React.Dispatch<SetStateAction<Music | null>>;
 };
 
-const MusicList = ({ handleClick, setSelectedIdx, selectedIdx, musicList }: Props) => {
+const MusicList = ({ handleClick, musicList, selectedMusic, setSelectedMusic }: Props) => {
   const groupRef = useRef<THREE.Group>(null!);
   const three = useThree();
   const scroll = useScroll();
@@ -25,12 +25,13 @@ const MusicList = ({ handleClick, setSelectedIdx, selectedIdx, musicList }: Prop
     <group ref={groupRef}>
       {musicList?.map((music, i) => (
         <MusicCard
-          music={music}
           key={i}
+          music={music}
           index={i}
           handleClick={handleClick}
-          setSelectedIdx={setSelectedIdx}
-          selectedIdx={selectedIdx}
+          selectedMusic={selectedMusic}
+          setSelectedMusic={setSelectedMusic}
+          musics={musicList}
           groupY={scroll.offset * three.viewport.height * 0.25}
         />
       ))}
