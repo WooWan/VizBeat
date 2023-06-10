3bfcf981eeef120245bc0deaa4302da5d635042b
WooWan <woohobi@gmail.com>
2023-06-11 02:23:01 +0900

    fix: 화면 크기에 따라 재생 버튼 위치가 달라지는 문제 해결

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,11 +1,15 @@
 import * as THREE from 'three';
 import { TextureLoader } from 'three';
 import { useFrame, useLoader } from '@react-three/fiber';
-import { useScroll } from '@react-three/drei';
+import { Html, useScroll } from '@react-three/drei';
 import React, { useEffect, useRef, useState } from 'react';
 import { lerp } from 'three/src/math/MathUtils';
 import { MeshAxis, YAxis } from '@/types/Axis';
 import { Music } from '@prisma/client';
+import { Play } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { useRouter } from 'next/router';
+import { useMusicPlayStore } from '@/store/music';
 
 const radian = Math.PI / 180;
 
@@ -21,13 +25,21 @@ type Props = {
 
 const LERP_FACTOR = 0.05;
 
-const Music = ({ music, index, handleClick, groupY, selectedMusic, musics, setSelectedMusic }: Props) => {
+const MusicAlbum = ({ music, index, handleClick, groupY, selectedMusic, musics, setSelectedMusic }: Props) => {
   const [originalPosition] = useState(2 - index * 1.5);
   const [rotation, setRotation] = useState(radian * 10 * index);
+  const setIsMusicPlay = useMusicPlayStore((state) => state.setIsMusicPlay);
   const meshRef = useRef<THREE.Mesh>(null!);
   const cover = useLoader(TextureLoader, music.albumCover);
   const texture = useLoader(TextureLoader, '/cdtexture.jpg');
   const scroll = useScroll();
+  const router = useRouter();
+
+  const handlePlayMusic = () => {
+    if (selectedMusic?.id !== music.id) return;
+    setIsMusicPlay(true);
+    router.push('/stage');
+  };
 
   const updateRotation = ({ x, y, z }: MeshAxis) => {
     meshRef.current.rotation.x = lerp(meshRef.current.rotation.x, x, LERP_FACTOR);
@@ -88,6 +100,21 @@ const Music = ({ music, index, handleClick, groupY, selectedMusic, musics, setSe
         handleClick(music.id);
       }}
     >
+      <Html>
+        <div
+          onClick={handlePlayMusic}
+          className={cn(
+            'absolute left-1/2 top-1/2 flex h-24 w-24 -translate-x-1/2 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full bg-white/[0.7] opacity-0',
+            {
+              'opacity-100': selectedMusic?.id === music.id
+            }
+          )}
+        >
+          <button>
+            <Play size={48} className={'fill-current text-black/[0.85]'} />
+          </button>
+        </div>
+      </Html>
       <boxGeometry args={[10, 0.7, 10]} />
       <meshBasicMaterial attach="material-0" map={texture} />
       <meshBasicMaterial attach="material-1" map={texture} />
@@ -98,4 +125,4 @@ const Music = ({ music, index, handleClick, groupY, selectedMusic, musics, setSe
     </mesh>
   );
 };
-export default Music;
+export default MusicAlbum;
