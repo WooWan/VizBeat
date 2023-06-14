22888cdb45d1afa3bbbe1e1b5f1ff705a08d6a1f
WooWan <woohobi@gmail.com>
2023-06-14 11:19:16 +0900

    add: music, stage constant

---
+++ src/constants/music.ts
@@ -0,0 +1,34 @@
+export const RADIAN = Math.PI / 180;
+
+export const instruments = [
+  {
+    position: [32, -10, -20],
+    rotation: [0, RADIAN * -20, 0],
+    scale: [18, 18, 18],
+    url: '/gltf/drum/scene.gltf'
+  },
+  {
+    position: [-32, -10, -16],
+    rotation: [0, RADIAN * 10, 0],
+    scale: [0.5, 0.5, 0.5],
+    url: '/gltf/piano/scene.gltf'
+  },
+  {
+    position: [73, -6, 12],
+    rotation: [0, RADIAN * -0.75, 0],
+    scale: [2.25, 2.25, 2.25],
+    url: '/gltf/guitar1/scene.gltf'
+  },
+  {
+    position: [-75, -10, 8],
+    rotation: [0, RADIAN * 30, 0],
+    scale: [23, 23, 23],
+    url: '/gltf/guitar/scene.gltf'
+  },
+  {
+    position: [0, -15, 30],
+    rotation: [0, 0, 0],
+    scale: [0.3, 0.3, 0.3],
+    url: '/gltf/microphone/scene.gltf'
+  }
+];
---
+++ src/constants/stage.ts
@@ -0,0 +1,13 @@
+export const stages = [
+  {
+    position: [10, -10, 0],
+    scale: [0.12, 0.02, 0.04],
+    url: '/gltf/substance_wood_floor_material_pbr/scene.gltf'
+  },
+  {
+    position: [30, -10, 30],
+    rotation: [0, Math.PI / 2, 0],
+    scale: [0.02, 0.02, 0.03],
+    url: '/gltf/old_garage/scene.gltf'
+  }
+];
