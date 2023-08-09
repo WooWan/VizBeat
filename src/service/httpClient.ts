73fcb3d61f4f08eba70d7696e86916236a142c76
WooWan <woohobi@gmail.com>
2023-08-09 18:57:32 +0900

    refactor: get token before api request

--- src/service/httpClient.ts
+++ src/service/httpClient.ts
@@ -11,38 +11,28 @@ export const httpClient = axios.create({
 
 export const spotifyClient = () => {
   const instance = axios.create({
-    baseURL: 'https://api.spotify.com/v1'
+    baseURL: process.env.NEXT_PUBLIC_SPOTIFY_API_URL
   });
 
-  instance.interceptors.response.use(
-    (res) => res,
-    async (error) => {
-      const {
-        config,
-        response: { status }
-      } = error;
-      if (status === 401) {
-        const originalRequest = config;
-        const newToken = await axios.post(
-          'https://accounts.spotify.com/api/token',
-          new URLSearchParams({
-            grant_type: 'client_credentials',
-            client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID!,
-            client_secret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET!
-          }),
-          {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            }
-          }
-        );
-        if (newToken) {
-          originalRequest.headers.Authorization = `Bearer ${newToken.data.access_token}`;
+  instance.interceptors.request.use(async (config) => {
+    const token = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      new URLSearchParams({
+        grant_type: 'client_credentials',
+        client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID!,
+        client_secret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET!
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
         }
-        return axios(originalRequest);
       }
-      return Promise.reject(error);
+    );
+    if (token) {
+      config.headers.Authorization = `Bearer ${token.data.access_token}`;
     }
-  );
+    return config;
+  });
+
   return instance;
 };
