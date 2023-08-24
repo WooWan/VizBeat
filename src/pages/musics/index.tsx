2f5d71aa4aa37f6a792acdf4b0b91e0e16119f49
Changwan <47740690+WooWan@users.noreply.github.com>
2023-08-24 17:32:31 +0900

    design: remove music icon

--- src/pages/musics/index.tsx
+++ src/pages/musics/index.tsx
@@ -4,7 +4,6 @@ import { Music } from '@prisma/client';
 import MusicsNavbar from '@/components/layout/MusicsNavbar';
 import MusicsCanvas from '@/canvas/MusicsCanvas';
 import { cn } from '@/lib/utils';
-import { Music4Icon } from 'lucide-react';
 import Link from 'next/link';
 import { useMusicPlayStore } from '@/store/music';
 
@@ -31,14 +30,13 @@ const MusicsPage = () => {
           href="/stage"
           onClick={() => setIsMusicPlay(true)}
           className={cn(
-            'absolute left-1/2 top-1/2 z-50 hidden h-12 w-12 translate-x-16 translate-y-20 items-center justify-center rounded-full bg-zinc-950/[0.7] text-white lg:h-16 lg:w-48',
+            'absolute left-1/2 top-1/2 z-50 hidden h-12 w-12 translate-x-16 translate-y-12 items-center justify-center rounded-full bg-zinc-950/[0.85] p-2 lg:h-36 lg:w-36',
             {
-              'flex items-center justify-center gap-x-2.5': selectedMusic
+              'flex items-center justify-center gap-x-2': selectedMusic
             }
           )}
         >
-          <Music4Icon className="h-6 w-6 text-rose-100 lg:h-7 lg:w-7" />
-          <span className="text-lg font-bold">Go to Stage</span>
+          <span className="text-md text-cente font-bold text-white">Go to Stage</span>
         </Link>
         <Suspense>
           <MusicsCanvas
