bcabb97b97d9462596fa6a2823e414d53d2539c0
Changwan <47740690+WooWan@users.noreply.github.com>
2023-08-30 18:04:36 +0900

    remove: axios acess control option

--- src/service/apiClient.ts
+++ src/service/apiClient.ts
@@ -4,7 +4,6 @@ export const nextClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': `application/json;charset=UTF-8`,
-    'Access-Control-Allow-Origin': '*',
     Accept: 'application/json'
   }
 });
