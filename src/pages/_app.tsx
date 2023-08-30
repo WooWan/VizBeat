8ca5e4a977ba64192a06cf6c81954eb0b42e7c4b
Changwan <47740690+WooWan@users.noreply.github.com>
2023-08-30 20:05:02 +0900

    remove: next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
 import TanstackQueryProvider from '@/components/providers/TanstackQueryProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <TanstackQueryProvider>
-        <div className="h-screen">
-          <Component {...pageProps} />
-        </div>
-      </TanstackQueryProvider>
-    </SessionProvider>
+    <TanstackQueryProvider>
+      <div className="h-screen">
+        <Component {...pageProps} />
+      </div>
+    </TanstackQueryProvider>
   );
 }
