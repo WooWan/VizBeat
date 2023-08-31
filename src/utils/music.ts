c5b8cad6b1cfb6e8dc4c5ae1ecaa740f8b7fa444
Changwan <47740690+WooWan@users.noreply.github.com>
2023-08-31 15:11:36 +0900

    update: solo 트랙일 때 색상 변경

--- src/utils/music.ts
+++ src/utils/music.ts
@@ -1,4 +1,5 @@
-import { AudioData } from '@/store/types';
+import { AudioData, Instruments } from '@/store/types';
+import { InstrumentData, InstrumentType } from '@/types/instrument';
 
 export const mockData = (): AudioData => ({ gain: 1, data: new Uint8Array(), source: {} as AudioBufferSourceNode });
 
@@ -27,4 +28,10 @@ export const createAudio = async (url: string): Promise<AudioData> => {
     data,
     gain: 1
   };
+};
+
+export const calculateIsSolo = (currentType: InstrumentType, instrumentState: Instruments) => {
+  if (instrumentState[currentType].isMuted) return false;
+
+  return Object.entries(instrumentState).every(([type, state]) => type === currentType || state.isMuted);
 };
