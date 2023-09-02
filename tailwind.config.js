2a40787cf8a3402c9e96d3b8bda04599cb3acf72
WooWan <woohobi@gmail.com>
2023-09-02 13:00:43 +0900

    config: remove duplicate animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -74,10 +74,6 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
       }
     }
   },
