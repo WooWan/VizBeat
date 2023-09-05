95f4e309fed7f47d410b85b854dc7e301358cd63
qjarl592 <81066891+qjarl592@users.noreply.github.com>
2023-09-05 20:48:28 +0900

    Update Bar.tsx

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -31,11 +31,11 @@ export default function Bar({ radius, index, centerPos, position, theta, color, 
     } else {
       heightRef.current = height - height * 0.4;
     }
-    angleRef.current = (angleRef.current + radian) % 360;
     const power = mean < 0 ? 0 : mean * 10;
-    const nx = centerPos[0] + (radius + Math.round(power * 5) * 10) * Math.cos(angleRef.current);
+    angleRef.current = (angleRef.current + radian) % 360;
+    const nx = centerPos[0] + (radius + Math.round(power * 5) * 8) * Math.cos(angleRef.current);
     const ny = centerPos[1] + Math.log(1 + heightRef.current * 200) * 10;
-    const nz = centerPos[2] + (radius + Math.round(power * 5) * 10) * Math.sin(angleRef.current);
+    const nz = centerPos[2] + (radius + Math.round(power * 5) * 8) * Math.sin(angleRef.current);
 
     barRef.current.position.x = lerp(barRef.current.position.x, nx, 0.02);
     barRef.current.position.y = lerp(barRef.current.position.y, ny, 0.03);
