67ae27d1663d3093e3430869581ec9188c2b56b8
WooWan <woohobi@gmail.com>
2023-09-06 22:11:21 +0900

    simplify logic

--- src/components/Rig.tsx
+++ src/components/Rig.tsx
@@ -1,18 +1,17 @@
-import { useFrame, useThree } from '@react-three/fiber';
-import React, { ReactNode, useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
 import { Vector3 } from 'three';
 
 type Props = {
-  children: ReactNode;
+  children: React.ReactNode;
 };
 
 export default function Rig({ children }: Props) {
   const groupRef = useRef<any>(null);
   const vector = new Vector3();
-  const { camera, mouse } = useThree();
 
-  useFrame(() => {
-    camera.position.lerp(vector.set(-mouse.x * 10, 20 + -mouse.y * 5, 70), 0.05);
+  useFrame((state) => {
+    state.camera.position.lerp(vector.set(-state.mouse.x * 10, 20 + -state.mouse.y * 5, 70), 0.05);
   });
 
   return <group ref={groupRef}>{children}</group>;
