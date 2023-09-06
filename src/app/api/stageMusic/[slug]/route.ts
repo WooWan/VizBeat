b840fd4f9048b0c0784d89e0cb7f0a886d544722
qjarl592 <81066891+qjarl592@users.noreply.github.com>
2023-09-06 12:39:09 +0900

    Update route.ts

--- src/app/api/stageMusic/[slug]/route.ts
+++ src/app/api/stageMusic/[slug]/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
-import { NextApiRequest } from 'next';
 
-export async function GET(req: NextApiRequest, { params }: { params: { slug: string } }) {
+export async function GET(req: Request, { params }: { params: { slug: string } }) {
   const res = await prisma.music.findUnique({
     where: {
       id: params.slug
