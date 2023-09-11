457e67a9ede1abd96e1e7e1b0fabc4c00dc898a5
WooWan <woohobi@gmail.com>
2023-09-11 19:25:13 +0900

    refactor: simplify fethcing audio from indexed db

--- src/hooks/queries/music/useMusics.ts
+++ src/hooks/queries/music/useMusics.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { musicKey } from '@/hooks/queries/music/queryKeys';
 import { fetchMusicFromYoutube, fetchMusics, separateMusic, fetchMusic } from '@/service/musics';
+import { fetchAndStoreMusic } from '@/utils/fetchMusicIdb';
 
 export const useMusics = () => {
   return useQuery({
@@ -29,5 +30,23 @@ export const useMusicSearch = (keyword: string) => {
     queryFn: () => fetchMusicFromYoutube({ keyword }),
     enabled: !!keyword,
     retry: 0
+  });
+};
+
+export const useAudioTracks = (id: string) => {
+  const { data: music } = useMusic(id);
+
+  return useQuery({
+    queryKey: ['audio-tracks', id],
+    queryFn: async () =>
+      await Promise.all([
+        fetchAndStoreMusic(music?.vocalUrl),
+        fetchAndStoreMusic(music?.drumUrl),
+        fetchAndStoreMusic(music?.guitarUrl),
+        fetchAndStoreMusic(music?.bassUrl),
+        fetchAndStoreMusic(music?.pianoUrl),
+        fetchAndStoreMusic(music?.otherUrl)
+      ]),
+    enabled: !!music
   });
 };
--- src/utils/fetchMusicIdb.ts
+++ src/utils/fetchMusicIdb.ts
@@ -1,33 +1,7 @@
-import { Music } from '@prisma/client';
 import { IndexedDB } from './indexedDB';
 
 const dbName = 'db';
 const tableName = 'musics';
-
-export async function fetchMusicsStage(music: Music) {
-  const id = music.id;
-  const idb = new IndexedDB(dbName);
-  await idb.init();
-  await idb.createTable(tableName);
-  const keys = (await idb.getAllKey(tableName)) as string[];
-  //vocal
-  if (!keys.includes(id)) {
-    const vocalBlob = await createBlobFromURL(music.vocalUrl);
-    const drumBlob = await createBlobFromURL(music.drumUrl);
-    const guitarBlob = await createBlobFromURL(music.guitarUrl);
-    const bassBlob = await createBlobFromURL(music.bassUrl);
-    const pianoBlob = await createBlobFromURL(music.pianoUrl);
-    const allBlobs = { vocal: vocalBlob, drum: drumBlob, guitar: guitarBlob, bass: bassBlob, piano: pianoBlob };
-    await idb.putValue(tableName, allBlobs, id);
-  }
-  const { vocal, drum, guitar, bass, piano } = await idb.getValue(tableName, id);
-  const vocalAudio = new Audio(URL.createObjectURL(vocal));
-  const drumAudio = new Audio(URL.createObjectURL(drum));
-  const guitarAudio = new Audio(URL.createObjectURL(guitar));
-  const bassAudio = new Audio(URL.createObjectURL(bass));
-  const pianoAudio = new Audio(URL.createObjectURL(piano));
-  return [vocalAudio, drumAudio, guitarAudio, bassAudio, pianoAudio];
-}
 
 async function createBlobFromURL(musicUrl: string | null) {
   if (!musicUrl) return;
@@ -41,16 +15,26 @@ async function createBlobFromURL(musicUrl: string | null) {
   }
 }
 
-export async function fetchAndStoreMusic(url: string) {
-  const idb = new IndexedDB(dbName);
-  await idb.init();
-  await idb.createTable(tableName);
-
-  const keys = (await idb.getAllKey(tableName)) as string[];
-  if (!keys.includes(url)) {
-    const blob = await createBlobFromURL(url);
-    await idb.putValue(tableName, blob, url);
+export async function fetchAndStoreMusic(url?: string | null): Promise<Blob> {
+  if (!url) {
+    throw new Error('url is undefined');
   }
 
-  return await idb.getValue(tableName, url);
+  try {
+    const idb = new IndexedDB(dbName);
+    await idb.init();
+    await idb.createTable(tableName);
+
+    const keys = (await idb.getAllKey(tableName)) as string[];
+
+    if (!keys.includes(url)) {
+      const blob = await createBlobFromURL(url);
+      await idb.putValue(tableName, blob, url);
+    }
+
+    return await idb.getValue(tableName, url);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
