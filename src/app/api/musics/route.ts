20b5389881a07ba550de3ac55a81092e6677ec94
WooWan <woohobi@gmail.com>
2023-09-14 00:57:33 +0900

    use server only env variables

--- src/app/api/musics/route.ts
+++ src/app/api/musics/route.ts
@@ -6,7 +6,7 @@ export const revalidate = 0;
 
 export async function GET(request: NextRequest) {
   const ipAddress = request.headers.get('x-forwarded-for');
-  const userId = generateUserId(ipAddress ?? process.env.NEXT_PUBLIC_IP_ADDRESS_DEFAULT);
+  const userId = generateUserId(ipAddress ?? process.env.IP_ADDRESS_DEFAULT);
   const defaultId = generateUserId();
 
   const res = await prisma.music.findMany({
