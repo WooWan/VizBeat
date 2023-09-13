66ae90e294c2f0bc455486eff85d902d7ccfcc38
WooWan <woohobi@gmail.com>
2023-09-13 21:13:01 +0900

    feat: fetch musics by ip address

--- src/app/api/musics/route.ts
+++ src/app/api/musics/route.ts
@@ -1,12 +1,27 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
+import { generateUserId } from '@/utils/password';
 
 export const revalidate = 0;
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const ipAddress = request.headers.get('x-forwarded-for');
+  const userId = generateUserId(ipAddress ?? '0.0.0.0');
+  const defaultId = generateUserId();
+
   const res = await prisma.music.findMany({
     orderBy: {
       createdAt: 'asc'
+    },
+    where: {
+      OR: [
+        {
+          userId
+        },
+        {
+          userId: defaultId
+        }
+      ]
     }
   });
   return NextResponse.json(res);
