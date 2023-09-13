ddd97335f876bb348b929d3b97e8a58100103510
WooWan <woohobi@gmail.com>
2023-09-13 15:50:43 +0900

    update: opt out caching and sort

--- src/app/api/musics/route.ts
+++ src/app/api/musics/route.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
+export const revalidate = 0;
+
 export async function GET() {
-  const res = await prisma.music.findMany();
+  const res = await prisma.music.findMany({
+    orderBy: {
+      createdAt: 'asc'
+    }
+  });
   return NextResponse.json(res);
 }
