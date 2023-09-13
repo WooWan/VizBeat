066c994a0ae894c692b44b3a8a17ec17419aeff9
WooWan <woohobi@gmail.com>
2023-09-13 10:34:19 +0900

    feat: revalidate after music separation

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
+
+export async function POST(request: NextRequest) {
+  const token = request.nextUrl.searchParams.get('token');
+
+  if (token !== process.env.REVALIDATE_TOKEN) {
+    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 });
+  }
+
+  revalidatePath('musics');
+  return NextResponse.json({ revalidated: true, now: Date.now() });
+}
