5aabfd470a33f153e43b256bbaeaa5764bc102b7
WooWan <woohobi@gmail.com>
2023-09-14 17:06:57 +0900

    feat: 음원 검색 실패할 경우, 에러 바운더리로 에러 핸들링

--- src/components/MusicSearch.tsx
+++ src/components/MusicSearch.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image';
 import { useMusicSearch } from '@/hooks/queries/music/useMusics';
 import { cn } from '@/lib/utils';
 import { MusicUpload, YoutubeMusic } from '@/types/music';
-import { RefreshCwIcon, XIcon } from 'lucide-react';
+import { XIcon } from 'lucide-react';
 import { useDebounce } from '@/hooks/useDebounce';
-import { Suspense, useState } from 'react';
+import { useState } from 'react';
 import { Input } from '@/components/ui/input';
+import { SearchFallback } from '@/components/error-boundary/fallback/SearchFallback';
+import ApiErrorBoundary from '@/components/error-boundary/ApiBoundary';
 
 type Props = {
   updateSelectedTrack: (track: YoutubeMusic) => void;
@@ -15,7 +17,6 @@ type Props = {
 function MusicSearch({ updateSelectedTrack, selectedTrack }: Props) {
   const [musicKeyword, setMusicKeyword] = useState('');
   const debouncedKeyword = useDebounce(musicKeyword, 200);
-  const { data, isError, refetch } = useMusicSearch(debouncedKeyword);
 
   const handleSearchMusic = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMusicKeyword(e.target.value);
@@ -37,42 +38,42 @@ function MusicSearch({ updateSelectedTrack, selectedTrack }: Props) {
           </button>
         )}
       </div>
-        <ul
-          className={cn('flex max-h-[175px] flex-col overflow-y-scroll  border-zinc-300', {
-            'border-b-[1px]': data?.length
+      <ApiErrorBoundary renderFallback={SearchFallback} resetKeys={[debouncedKeyword]}>
+        <MusicList updateSelectedTrack={updateSelectedTrack} selectedTrack={selectedTrack} keyword={debouncedKeyword} />
+      </ApiErrorBoundary>
+    </section>
+  );
+}
+
+type MusicSearchProps = Props & {
+  keyword: string;
+};
+
+function MusicList({ updateSelectedTrack, selectedTrack, keyword }: MusicSearchProps) {
+  const { data } = useMusicSearch(keyword);
+
+  return (
+    <ul
+      className={cn('flex max-h-[175px] flex-col overflow-y-scroll  border-zinc-300', {
+        'border-b-[1px]': data?.length
+      })}
+    >
+      {data?.map((track) => (
+        <li
+          key={track.id}
+          onClick={() => updateSelectedTrack(track)}
+          className={cn('flex h-[70px] gap-x-3.5 rounded-sm border-b-[1px] py-3 pl-3 hover:bg-gray-200', {
+            'bg-gray-200': selectedTrack?.id === track.id
           })}
         >
-          {data?.map((track) => (
-            <li
-              key={track.id}
-              onClick={() => updateSelectedTrack(track)}
-              className={cn('flex h-[70px] gap-x-3.5 rounded-sm border-b-[1px] py-3 pl-3 hover:bg-gray-200', {
-                'bg-gray-200': selectedTrack?.id === track.id
-              })}
-            >
-              <Image
-                className="rounded-md object-contain"
-                width={50}
-                height={50}
-                src={track.thumbnail}
-                alt="album-cover"
-              />
-              <div className="break-keep">
-                <h5 className="text-sm font-semibold">{track.parsed_artist}</h5>
-                <span className="line-clamp-1 text-sm">{track.parsed_title}</span>
-              </div>
-            </li>
-          ))}
-        </ul>
-      {isError && (
-        <div className="relative flex h-20 w-full items-center justify-center gap-x-1 rounded-md bg-gray-100/[0.75]">
-          <span className="text-gray-400">No results</span>
-          <button type="button" onClick={() => refetch()}>
-            <RefreshCwIcon className="h-4 w-4  text-gray-400" />
-          </button>
-        </div>
-      )}
-    </section>
+          <Image className="rounded-md object-contain" width={50} height={50} src={track.thumbnail} alt="album-cover" />
+          <div className="break-keep">
+            <h5 className="text-sm font-semibold">{track.parsed_artist}</h5>
+            <span className="line-clamp-1 text-sm">{track.parsed_title}</span>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
 
