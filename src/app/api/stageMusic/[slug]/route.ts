039c6316d92200078c4f4ac7d1948fea1ec1087c
WooWan <woohobi@gmail.com>
2023-09-19 15:25:26 +0900

    disable route caching

--- src/app/api/stageMusic/[slug]/route.ts
+++ src/app/api/stageMusic/[slug]/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
+export const revalidate = 0;
+
 export async function GET(req: Request, { params }: { params: { slug: string } }) {
   const res = await prisma.music.findUnique({
     where: {
