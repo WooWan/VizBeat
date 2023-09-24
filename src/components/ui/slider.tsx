1030c4835d05b63991abdbf0f7844d88888c1ea7
WooWan <woohobi@gmail.com>
2023-09-24 18:07:45 +0900

    design: update slider color

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -13,7 +13,7 @@ const Slider = React.forwardRef<
     {...props}
   >
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-yellow-50">
-      <SliderPrimitive.Range className="absolute h-full bg-amber-200" />
+      <SliderPrimitive.Range className="absolute h-full bg-amber-300" />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb className="block h-3.5 w-3.5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
