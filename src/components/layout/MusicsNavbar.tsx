ec1da41c182182b36b535f3ea9fe113340521a8d
qjarl592 <81066891+qjarl592@users.noreply.github.com>
2023-09-25 20:47:34 +0900

    remove unnecessary code

--- src/components/layout/MusicsNavbar.tsx
+++ src/components/layout/MusicsNavbar.tsx
@@ -31,7 +31,6 @@ const MusicsNavbar = () => {
   const redirectToStage = () => {
     api.stopAudio();
     router.push(`/stage/${musicInfo?.id}`);
-    api.clear();
   };
 
   const selectAlbum = (music: Music) => {
