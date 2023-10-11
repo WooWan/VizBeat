f845c8f1f367fc62e57db6ffa740cc7ca1f25945
WooWan <woohobi@gmail.com>
2023-10-11 10:52:39 +0900

    fix: dynamic import hotjar

--- src/hooks/useHotjar.ts
+++ src/hooks/useHotjar.ts
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
-import { hotjar } from 'react-hotjar';
 
 function useHotjar() {
   useEffect(() => {
     if (process.env.NODE_ENV !== 'development') {
-      hotjar.initialize(Number(process.env.NEXT_PUBLIC_HOTJAR_ID), Number(process.env.NEXT_PUBLIC_HJSV));
+      import('react-hotjar').then((hotjarLib) => {
+        hotjarLib.hotjar.initialize(Number(process.env.NEXT_PUBLIC_HOTJAR_ID), Number(process.env.NEXT_PUBLIC_HJSV));
+      });
     }
   }, []);
 }
