190709b9c6e05d437d5a394a8d9ba037faac1ee1
WooWan <woohobi@gmail.com>
2023-10-15 02:42:15 +0900

    add: cta 버튼

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,6 +1,9 @@
 import { motion } from 'framer-motion';
 import { slideIn, staggerContainer, textVariant } from '@/utils/motion';
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import MusicIcon from '@/icons/MusicIcon';
+import Link from 'next/link';
 
 const Hero = () => (
   <section className={`xs:py-8 py-12 pl-6 sm:py-16 sm:pl-16`}>
@@ -20,7 +23,7 @@ const Hero = () => (
         >
           VizBeats
         </motion.h1>
-        <motion.div variants={textVariant(1.2)} className="flex flex-row items-center justify-center">
+        <motion.div variants={textVariant(1.2)} className="flex flex-row items-center justify-center pb-6">
           <span
             className={
               'text-[28px] font-medium uppercase leading-[41px] text-white sm:text-[20px] sm:leading-[48px] md:text-[66px] md:leading-[70px] lg:text-[96px] lg:leading-[105px]'
@@ -29,7 +32,16 @@ const Hero = () => (
             Visualizers
           </span>
         </motion.div>
+        <motion.div variants={textVariant(1.2)} className="flex items-center justify-center">
+          <Link href={'/musics'}>
+            <Button className="flex items-center gap-x-3 rounded-[32px] bg-[#25618B] px-6 py-6">
+              <MusicIcon className="h-6 w-6 text-white" />
+              <span className="text-[16px] font-normal text-white sm:text-lg md:text-sm lg:text-xl">시작하기</span>
+            </Button>
+          </Link>
+        </motion.div>
       </div>
+
       <motion.div
         variants={slideIn('right', 'tween', 0.2, 1)}
         className="relative -mt-[12px] min-h-[500px] w-full md:-mt-[20px]"
